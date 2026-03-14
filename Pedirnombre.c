5d39d78cd3dca7169397bb30e9fd0ec9b2b67718
joserodriguez4263-cyber <jose.rodriguez4263@alumnos.udg.mx>
2026-03-13 23:01:34 -0600

    Create Pedirnombre.c

---
+++ Pedirnombre.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char nombre[20];
+
+    printf("Ingresa tu nombre:");
+    scanf("%19s", nombre);
+
+    printf("Tu nombre es:%s\n", nombre);
+
+return 0;
+
+}
